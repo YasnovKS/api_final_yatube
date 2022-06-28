d2d121f870dffd5e6a1c43dbb23e60e8d93b3edb
Яснов Кирилл Сергеевич <YasnovKS@snc.local>
2022-06-28 16:24:07 +0500

    Реализована работа posts

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from api.views import PostViewSet
+
+router = routers.SimpleRouter()
+router.register('posts', PostViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,21 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from posts.models import Post
+from .serializers import PostSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from .permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    '''Класс представления для объектов модели Post.
+    Реализует возможность получения, добавления, редактирования
+    и удаления постов пользователей.'''
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
